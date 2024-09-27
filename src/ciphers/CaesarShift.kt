0130c4c30281bc7be9268daf0f4a6ccc240f77dd
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-09-27 12:01:55 +0100

    Initial commit

---
+++ src/ciphers/CaesarShift.kt
@@ -0,0 +1,17 @@
+package ciphers
+
+
+object CaesarShift {
+    private val DEFAULT_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    fun encrypt(text: String, offset: Int, alphabet: String = DEFAULT_ALPHABET): String = text.map { c ->
+        if (c.isUpperCase()) c.shiftBy(offset, alphabet)
+        else c.shiftBy(offset, alphabet).lowercaseChar()
+    }.joinToString("")
+
+    fun decrypt(text: String, offset: Int, alphabet: String = DEFAULT_ALPHABET): String =
+        encrypt(text, -offset, alphabet)
+
+    private fun Char.shiftBy(n: Int, alphabet: String): Char =
+        alphabet[(alphabet.indexOf(this, ignoreCase = true) + n) % alphabet.length]
+}
